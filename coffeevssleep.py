3893de44cd04e92a13b4a5fecb15738d6729d77b
Justsneak7 <71436825+Justsneak7@users.noreply.github.com>
2021-06-10 19:51:19 +0530

    Add files via upload

---
+++ coffeevssleep.py
@@ -0,0 +1,8 @@
+import csv
+import plotly_express as px
+
+
+with open("data/cups of coffee vs hours of sleep.csv") as csv_file:
+    df=csv.DictReader(csv_file) 
+    fig=px.scatter(df, x="Coffee in ml",y="sleep in hours") 
+    fig.show()
